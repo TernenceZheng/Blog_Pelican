c5e56d9bd614348a0673f6fe04f438f6b4ad4944
JARVISZHENG\Jarvis Zheng <andy711023@hotmail.com>
2021-03-05 13:49:52 +0800

    調整功能

--- Pelican/pelicanconf.py
+++ Pelican/pelicanconf.py
@@ -3,7 +3,23 @@
 
 AUTHOR = 'Jarvis'
 SITENAME = 'Jarvis Blog'
-SITEURL = ''
+SITEURL = 'https://blog.godtable.top'
+SITELOGO = '/images/profile.png'
+FAVICON = '/images/favicon.ico'
+
+SITEMAP = {
+    "format": "xml",
+    "priorities": {
+        "articles": 0.5,
+        "indexes": 0.5,
+        "pages": 0.5
+    },
+    "changefreqs": {
+        "articles": "monthly",
+        "indexes": "daily",
+        "pages": "monthly"
+    }
+}
 
 PATH = 'content'
 
@@ -11,26 +27,55 @@ TIMEZONE = 'Asia/Taipei'
 
 DEFAULT_LANG = 'zh_TW'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# Add a link to your social media accounts
+SOCIAL = (
+    ('github', 'https://github.com/TernenceZheng'),
+    ('envelope','mailto:andy711023@gmail.com'),
+    ('linkedin','https://www.linkedin.com/in/jarvis-zheng-13a2b990'),
+    ('twitter','https://twitter.com/Jarvis_Zheng'),
+    ('facebook','https://www.facebook.com/andy.zheng.7967')
+)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = 'Flex'
+PLUGIN_PATHS = ['./pelican-plugins']
+
+PLUGINS = ['sitemap', 'neighbors']
+
+THEME = 'pelican-themes/Flex'
+
+STATIC_PATHS = ['images', 'extra']
+
+# Main Menu Items
+MAIN_MENU = True
+MENUITEMS = (('Archives', '/archives'),('Categories', '/categories'),('Tags', '/tags'))
+
+# Code highlighting the theme
+PYGMENTS_STYLE = 'friendly'
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}/'
+ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'
+
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = PAGE_URL + 'index.html'
+
+ARCHIVES_SAVE_AS = 'archives.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+
+# Feed generation is usually not desired when developing
+FEED_DOMAIN = SITEURL
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# HOME_HIDE_TAGS = True
+FEED_USE_SUMMARY = True
+
