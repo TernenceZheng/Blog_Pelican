4b34dac98d3462b13a1fec86da0854f843111cb5
JARVISZHENG\Jarvis Zheng <andy711023@gmail.com>
2021-03-06 13:12:08 +0800

    test

--- Pelican/pelicanconf.py
+++ Pelican/pelicanconf.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*- #
 
 AUTHOR = 'Jarvis'
-SITENAME = 'Jarvis Blog'
+SITENAME = 'Jarvis隨手筆記'
+
+SITESUBTITLE = "Web Developer"
+
+SITEDESCRIPTION = "紙上得來終覺淺，絕知此事要躬行"
+
 SITEURL = 'https://godtable.top'
 SITELOGO = '/images/profile.png'
 FAVICON = '/images/favicon.ico'
@@ -15,8 +20,7 @@ GOOGLE_ADSENSE = {
     'ca_id': 'ca-pub-4119678220307625',    # Your AdSense ID
     'page_level_ads': True,          # Allow Page Level Ads (mobile)
     'ads': {
-        'main_menu': '1234562',      # Banner before main menu (all pages)
-        'index_bottom': '1234564',   # Banner before footer (index only)
+        'article_bottom': '1234566', # Banner after article content (article only)
     }
 }
 
@@ -26,6 +30,7 @@ DISQUS_SITENAME = "jarvissui-shou-bi-ji"
 
 ADD_THIS_ID = "ra-6041ec511f4e3460"
 
+COPYRIGHT_YEAR = 2021
 
 SITEMAP = {
     "format": "xml",
@@ -47,10 +52,6 @@ TIMEZONE = 'Asia/Taipei'
 
 DEFAULT_LANG = 'zh_TW'
 
-LINKS = (
-    ("隨手筆記", "https://godtable.top"),
-    ("紙上得來終覺淺，絕知此事要躬行","")
-)
 
 # Social widget
 # Add a link to your social media accounts
@@ -103,10 +104,3 @@ AUTHOR_FEED_RSS = None
 # HOME_HIDE_TAGS = True
 FEED_USE_SUMMARY = True
 
-
-THEME_COLOR = 'light'
-THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
-THEME_COLOR_ENABLE_USER_OVERRIDE = True
-
-PYGMENTS_STYLE = 'emacs'
-PYGMENTS_STYLE_DARK = 'monokai'
